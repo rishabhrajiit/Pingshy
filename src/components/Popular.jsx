1689f8d26b2a0b10c9ae5c7b3ac5722da6f74c07
rishabhrajiit <rishabhraj481@gmail.com>
2025-11-12 11:02:23 +0530

    added community svg

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PostCard from './PostCard';
-
+import { getHeadlines } from '../../utils/headlines';
+import { dataP } from '../../utils/pseudo';
 const posts = [
   { id: 1, title: 'Top Trending', content: 'Most liked post here!', likes: 100, comments: 24 },
   { id: 2, title: 'React Guide', content: 'Upvoted many times', likes: 86, comments: 16 }
 ];
 
 const Popular = () => {
+  const [generalFeed,setGeneralFeed] = useState([...dataP?.articles]);
+    useEffect(() => {
+    const fetchData = async () => {
+      const data = await getHeadlines("general");
+      setGeneralFeed(data);
+    };
+    fetchData();
+  }, []);
   return (
     <main className="feed">
       <h2>Popular Posts</h2>
-      {posts.sort((a, b) => b.likes - a.likes).map((post) => (
+      {generalFeed.map((post) => (
         <PostCard key={post.id} post={post} />
       ))}
     </main>
